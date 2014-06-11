8c4aab91bcb4d646cfdf28c08d3d5ed7980129df
fuwenhuai <imcore@fuwenhuai.local>
2014-06-11 15:43:31 +0800

    add

---
+++ test/test/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  test
+//
+//  Created by fuwenhuai on 14-6-11.
+//  Copyright (c) 2014年 test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
